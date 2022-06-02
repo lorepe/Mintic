bd3f6bb371ad748d730e4490cf46918f79ffbaa2
lorepe <loreperez.2003@hotmail.com>
2022-06-01 23:16:33 -0500

    notesClass

---
+++ Ciclo1/Fundamentos_de_programacion/MisionTIC-2022-R2-Ciclo1-Fundamentos_de_Programacion/clase17/PromedioNotas/MainSistemaNotas.py
@@ -0,0 +1,57 @@
+##################
+#Aplicación de notas 
+#(Intermediación con el mundo del problema)
+##################
+
+#Importar las clases que representan las entidades del mundo del problema
+from Nota import Nota
+from Materia import Materia
+from Profesor import Profesor
+#Sección principal de la aplicación
+###################################
+
+#Instanciar: crear los objetos tipo nota
+objetoNota1 = Nota(nota100=40,descripcion='Primer Parcial')
+objetoNota2 = Nota(nota100=50,descripcion='Segundo Parcial')
+objetoNota3 = Nota(nota100=39,descripcion='Tercer Parcial')
+objetoNota4 = Nota(nota100=76,descripcion='Cuarto Parcial')
+objetoNota5 = Nota(nota100=96,descripcion='Quinto Parcial')
+
+#Ilustración de uso y actualización de la clase, objeto y atributos
+# print('Nota 1 en escala cualitativa:',objetoNota1.notaCualitativa)
+# print('Nota 1 en escala cualitativa:',objetoNota1.getNotaCualitativa())
+# print('Fecha de registro Nota 1',objetoNota1.getFechaRegistro())
+# print('Nota 1 en escala de 5',objetoNota1.getNota5())
+# objetoNota4.descripcion = 'Nueva descripción'
+# objetoNota4.setDescripcion('Nueva descripción')
+# print(objetoNota4)
+
+
+#Pedir al objeto que se muestre en pantalla (consola)
+print("***Notas")
+objetoNota1.mostrarInfoNota()
+objetoNota2.mostrarInfoNota()
+objetoNota3.mostrarInfoNota()
+objetoNota4.mostrarInfoNota()
+objetoNota5.mostrarInfoNota()
+
+##Crear profesor
+profesor1=Profesor(nombre="Jose Faset")
+
+#Instanciar: crear los objetos tipo nota
+objetoMateria = Materia(nota1=objetoNota1, nota2=objetoNota2, nota3=objetoNota3, nota4=objetoNota4, nota5=objetoNota5,nombre="Estructura de Lenguajes", creditos=3,docenteEncargado=profesor1)
+
+#Realizar varias versiones de promedio para la materia
+objetoMateria.calcularPromedio5()
+objetoMateria.calcularPromedio100()
+objetoMateria.calcularPromedioAjustado5()
+objetoMateria.calcularPromedioAjustado100()
+
+#Solicitar a la materia que se muestre en pantalla
+print("***Materia (Respuesta al requerimiento)")
+objetoMateria.mostrarInfoMateria()
+
+#Ilustración de funcionalidad dinámica adicionada: atributo tipo lista para que la materia pueda contener más notas
+# objetoMateria.adicionarNota( Nota(nota100=100,descripcion='Adicional') )
+# print('Última adicionada')
+# objetoMateria.mostrarUltimaNota()
