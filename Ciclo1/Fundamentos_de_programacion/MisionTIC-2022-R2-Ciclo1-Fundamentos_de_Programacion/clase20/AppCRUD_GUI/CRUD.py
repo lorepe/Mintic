db957ab8d70db4539416875e992bd889a8b83578
lorepe <loreperez.2003@hotmail.com>
2022-06-08 09:49:10 -0500

    last class

--- Ciclo1/Fundamentos_de_programacion/MisionTIC-2022-R2-Ciclo1-Fundamentos_de_Programacion/clase20/AppCRUD_GUI/CRUD.py
+++ Ciclo1/Fundamentos_de_programacion/MisionTIC-2022-R2-Ciclo1-Fundamentos_de_Programacion/clase20/AppCRUD_GUI/CRUD.py
@@ -4,15 +4,15 @@
 import json
 
 #Adición de una tarea (Create)
-def Create(tareas, identificador, tareaNueva):    
-    tareas[identificador] = tareaNueva #Mutación del diccionario por referencia      
+def Create(tareas, identificador, tareaNueva):
+    tareas[identificador] = tareaNueva #Mutación del diccionario por referencia
 
 #Consultar la información de todas las tareas (Read)
 def Read(rutaArchivo='listadoTareas.json'):
 
-    #Cargar el listado de tareas a un diccionario desde la capa de datos (archivo JSON)    
+    #Cargar el listado de tareas a un diccionario desde la capa de datos (archivo JSON)
     diccionarioTareas = {}#Contenedor del listado de tareas que gestiona la App
-    try:    
+    try:
         with open(rutaArchivo) as archivo:
             diccionarioTareas = json.load(archivo)
     except:
@@ -25,7 +25,7 @@ def Read(rutaArchivo='listadoTareas.json'):
 #Actualizar una tarea específica (Update)
 def Update(tareas, identificador, tareaActualizada):
     #Revisar los campos que llegan con información para actualizar
-    for id_campo, campo in tareaActualizada.items():        
+    for id_campo, campo in tareaActualizada.items():
         if campo:
             tareas[identificador][id_campo] = campo #Actualizar el campo del diccionario (referencia)
 
@@ -42,6 +42,6 @@ def Write(tareas, rutaArchivo='listadoTareas.json'):
     except:
         print("Error: No se pudo guardar la información en la capa de datos.")
         return False
-    
+
     #Si la escrutra fue exitosa retorno correspondiente
     return True
