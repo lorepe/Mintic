2d05fb34baa0afa1118de47737e3c7d7d343dbc2
lorepe <loreperez.2003@hotmail.com>
2022-10-29 14:12:54 -0500

    proyecto pedidos

---
+++ Ciclo4/Ciclo 4A - Desarrollo De Aplicaciones Web - Grupo 30/LoopbackG2630/BackendPedidos/src/models/producto.model.ts
@@ -0,0 +1,49 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Producto extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  precio: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  imagen: string;
+
+  @property({
+    type: 'string',
+  })
+  pedidoId?: string;
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Producto>) {
+    super(data);
+  }
+}
+
+export interface ProductoRelations {
+  // describe navigational properties here
+}
+
+export type ProductoWithRelations = Producto & ProductoRelations;
